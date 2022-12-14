4703ed0da19fc13a2beffbd0116dd15788fd6f42
armodeniz <55904052+armodeniz@users.noreply.github.com>
2022-12-14 21:01:28 +0800

    Update BuildInInit.cs

--- LibsTestProj/Assets/BuildInInit.cs
+++ LibsTestProj/Assets/BuildInInit.cs
@@ -5,24 +5,6 @@ namespace XLua.LuaDLL
     public partial class Lua
     {
         [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int luaopen_rapidjson(System.IntPtr L);
-
-        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
-        public static int LoadRapidJson(System.IntPtr L)
-        {
-            return luaopen_rapidjson(L);
-        }
-
-        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int luaopen_lpeg(System.IntPtr L);
-
-        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
-        public static int LoadLpeg(System.IntPtr L)
-        {
-            return luaopen_lpeg(L);
-        }
-
-        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
         public static extern int luaopen_pb(System.IntPtr L);
 
         [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
@@ -31,13 +13,5 @@ namespace XLua.LuaDLL
             return luaopen_pb(L);
         }
 
-        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int luaopen_ffi(System.IntPtr L);
-
-        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
-        public static int LoadFFI(System.IntPtr L)
-        {
-            return luaopen_ffi(L);
-        }
     }
 }
